59aee5c11d8394513112bf33d5a83961729ad9d0
unknown <edward.porras@labinfo.is.escuelaing.edu.co>
2022-02-07 15:55:45 -0500

    heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 
-web: java $JAVA_OPTS -cp target/classes;target/dependency/*
+web: java -cp target/classes:target/dependency/* edu.escuelaing.arem.SparkWebApp
--- target/classes/public/index.js
+++ target/classes/public/index.js
@@ -7,7 +7,7 @@ var index = (function(){
         conectar : function(tipo, numero){
             console.log(tipo)
             console.log(numero)
-            var url = 'http://localhost:4567/Convertidor/'+ tipo + '/'+ numero;
+            var url = 'http://localhost:5000/Convertidor/'+ tipo + '/'+ numero;
             fetch(url)
 
                               .then(response => response.json())
